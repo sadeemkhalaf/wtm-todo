db5b71adbe3f00abf3c4eba4efb34f6d3b6d2d55
Sadeem Khalaf <sadeem@ahmads-MacBook-Pro.local>
2020-08-10 00:34:42 +0300

    server file needed dependencies added

--- server/server.js
+++ server/server.js
@@ -3,21 +3,30 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import Express from 'express';
+import cors from 'cors';
+import passport from 'passport';
 import bodyParser from 'body-parser';
 import todoController from './controller/todo.controller.js';
+import userController from './controller/user.controller.js';
+import jwtFunction from './config/passport.config.js';
 
 // Init an Express App.
 const app = new Express();
 dotenv.config();
 
 // Use your dependencies here
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+jwtFunction(passport);
+
 
 // use all controllers(APIs) here
+app.use('/', userController);
 app.use('/', todoController);
-app.use('/add-todo', todoController);
-app.use('/:id', todoController);
+
 
 // Start Server here
 app.listen(8080, () => {
@@ -33,10 +42,13 @@ app.listen(8080, () => {
 
     console.log('db is running')
   });
-  
-// const collection = client.db("wtmTodoDB").collection("TODOs");
-// Database admin:
-// admin: SA
-// password: FYVP7VGy182cUJIO
 
-// mongodb://SA:FYVP7VGy182cUJIO@ds257698.mlab.com:57698/node-auth 
+  app.use( (err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
