fb54dfa763ecedafff60d3956358346e7bcd006b
Sadeem Khalaf <46895880+sadeemkhalaf@users.noreply.github.com>
2020-08-15 21:03:22 +0300

    CORS issue

--- index.js
+++ index.js
@@ -51,6 +51,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
+
 
 console.log(JSON.stringify(import.meta));
 
