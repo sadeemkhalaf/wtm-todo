8031f4b4953461c2cc2abf5ec73228af58bcc4df
Sadeem Khalaf <46895880+sadeemkhalaf@users.noreply.github.com>
2020-08-16 17:08:23 +0300

    domain added

--- client/src/service/auth.service.js
+++ client/src/service/auth.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import authHeader from './auth.header';
 
-const API_URL = "http://localhost:5000/";
+// const API_URL = "http://localhost:5000/";
+const API_URL = "https://todo-app-wtm.herokuapp.com/"
 
 const Register = ({ name, email, password }) => {
   return axios.post(API_URL + "signup", {
--- client/src/service/todo.service.js
+++ client/src/service/todo.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import authHeader from './auth.header.js';
 
-const API_URL = "http://localhost:5000/";
+// const API_URL = "http://localhost:5000/";
+const API_URL = "https://todo-app-wtm.herokuapp.com/";
 
 const GetAllTodos = () => {
     return axios.get(API_URL, { headers: authHeader() });
